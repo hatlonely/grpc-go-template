d51e2724d6d0531ae01424e15d3d33fc2903ac0d
hatlonely <hatlonely@gmail.com>
2018-07-08 22:54:36 +0800

    update

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,15 +4,14 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 
-	"github.com/afex/hystrix-go/hystrix"
 	"github.com/hatlonely/grpc-go-template/api/addapi"
 	"github.com/hatlonely/grpc-go-template/pkg/grpchelper"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	_ "github.com/spf13/viper/remote"
-	"golang.org/x/time/rate"
 )
 
 func main() {
@@ -27,7 +26,9 @@ func main() {
 	}
 	config.BindPFlags(pflag.CommandLine)
 
+	// logrus
 	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
 
 	// create connection
 	conn, err := grpchelper.NewConn(config.Sub("conn"))
@@ -38,40 +39,36 @@ func main() {
 	defer conn.Close()
 
 	client := addapi.NewServiceClient(conn)
-	limiter := rate.NewLimiter(rate.Every(time.Duration(800)*time.Millisecond), 1)
-	hystrix.ConfigureCommand(
-		"grpc-go-template",
-		hystrix.CommandConfig{
-			Timeout:                100,
-			MaxConcurrentRequests:  2,
-			RequestVolumeThreshold: 4,
-			ErrorPercentThreshold:  25,
-			SleepWindow:            1000,
+
+	// init helper
+	helper := grpchelper.NewRPCHelper(config.Sub("helper"))
+
+	helper.SetCallback(
+		func(requestI interface{}) (interface{}, error) {
+			return client.Do(context.Background(), requestI.(*addapi.Request))
+		},
+		func(requestI interface{}) (interface{}, error) {
+			request := requestI.(*addapi.Request)
+			return &addapi.Response{
+				V: request.A + request.B,
+			}, nil
 		},
 	)
 
 	for i := 0; i < 10; i++ {
-		if err := limiter.Wait(context.Background()); err != nil {
-			panic(err)
-		}
 		request := &addapi.Request{
 			A: int64(rand.Intn(1000)),
 			B: int64(rand.Intn(1000)),
 		}
-		var response *addapi.Response
-		err := hystrix.Do("grpc-go-template", func() error {
-			var err error
-			ctx := context.Background()
-			response, err = client.Do(ctx, request)
-			return err
-		}, func(err error) error {
-			fmt.Println(err)
-			response = &addapi.Response{V: request.A + request.B}
-			return nil
-		})
-		logrus.WithField("request", request).WithField("response", response).Info()
-		if err != nil {
-			panic(err)
-		}
+
+		now := time.Now()
+		response, callErr, err := helper.Do(request)
+		logrus.WithFields(logrus.Fields{
+			"request":  request,
+			"response": response,
+			"callErr":  callErr,
+			"err":      err,
+			"costUs":   int(time.Since(now) / time.Microsecond),
+		}).Info()
 	}
 }
---
+++ pkg/grpchelper/helper.go
@@ -0,0 +1,96 @@
+package grpchelper
+
+import (
+	"context"
+	"time"
+
+	"github.com/afex/hystrix-go/hystrix"
+	"github.com/spf13/viper"
+	"golang.org/x/time/rate"
+)
+
+type limiterOption struct {
+	Interval time.Duration
+	Buckets  int
+}
+
+type hystrixOption struct {
+	Command                string
+	Timeout                time.Duration
+	MaxConcurrentRequests  int
+	RequestVolumeThreshold int
+	ErrorPercentThreshold  float64
+	SleepWindow            time.Duration
+}
+
+// NewRPCHelper create a new RPC Helper
+func NewRPCHelper(config *viper.Viper) *RPCHelper {
+	limiterOpt := &limiterOption{}
+	config.Sub("limiter").Unmarshal(limiterOpt)
+	hystrixOpt := &hystrixOption{}
+	config.Sub("hystrix").Unmarshal(hystrixOpt)
+
+	limiter := rate.NewLimiter(rate.Every(limiterOpt.Interval), limiterOpt.Buckets)
+	hystrix.ConfigureCommand(
+		hystrixOpt.Command,
+		hystrix.CommandConfig{
+			Timeout:                int(hystrixOpt.Timeout / time.Millisecond),
+			MaxConcurrentRequests:  hystrixOpt.MaxConcurrentRequests,
+			RequestVolumeThreshold: hystrixOpt.RequestVolumeThreshold,
+			ErrorPercentThreshold:  int(hystrixOpt.ErrorPercentThreshold * 100.0),
+			SleepWindow:            int(hystrixOpt.SleepWindow / time.Millisecond),
+		},
+	)
+	return &RPCHelper{
+		limiter: limiter,
+		command: hystrixOpt.Command,
+	}
+}
+
+// RPCHelper helper
+type RPCHelper struct {
+	limiter  *rate.Limiter
+	command  string
+	callback RPCFunc
+	fallback RPCFunc
+}
+
+// SetCallback set callback and fallback
+func (h *RPCHelper) SetCallback(callback, fallback RPCFunc) {
+	h.callback = callback
+	h.fallback = fallback
+}
+
+// Do do request
+func (h *RPCHelper) Do(request interface{}) (interface{}, error, error) {
+	var response interface{}
+	var err error
+	var callErr error
+
+	if err := h.limiter.Wait(context.Background()); err != nil {
+		return nil, nil, err
+	}
+
+	if h.fallback == nil {
+		err = hystrix.Do(h.command, func() error {
+			response, err = h.callback(request)
+			callErr = err
+			return err
+		}, nil)
+	} else {
+		err = hystrix.Do(h.command, func() error {
+			response, err = h.callback(request)
+			callErr = err
+			return err
+		}, func(inErr error) error {
+			callErr = inErr
+			response, err = h.fallback(request)
+			return err
+		})
+	}
+
+	return response, callErr, err
+}
+
+// RPCFunc helper function
+type RPCFunc func(request interface{}) (interface{}, error)
